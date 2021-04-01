4022adb40910bd307f6b9082e7e86fa2849fefa0
fernandasanchezg <fernandillago19892@hotmail.com>
2021-03-31 22:11:19 -0500

    Segundo cambio Alejandro y Fernanda

--- src/Cliente/Aplicacion.java
+++ src/Cliente/Aplicacion.java
@@ -1,6 +1,8 @@
 package Cliente;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import FuncionalidadesNotificadores.Funcionalidades;
@@ -11,65 +13,72 @@ import FuncionalidadesNotificadores.NotificadorSMS;
 import Libreria.EnviarNotificacion;
 import Libreria.Notificador;
 
+
+//En el main le pedimos al usuario que ingrese el mensaje y la opción por la cual quiere enviar este mensaje.
+
 public class Aplicacion {
 
+	private static final String List = null;
+
 	public static void main(String[] args) {
+
+		String mensaje;
+
 		Scanner teclado1 = new Scanner(System.in);
 		
-		
+		//Acá le pedimos al usuario que mensaje quiere enviar.
+
 		System.out.println("Cuál es el mensaje que quiere enviar?");
-		String mensaje = teclado1.next();
+		mensaje = teclado1.next();
+        
+		//Acá le pedimos el medio por el cual quiere enviar el mensaje
 		System.out.println("opción 1: Facebook");
 		System.out.println("opción 2: Correo personal");
 		System.out.println("opción 3: Correo empresarial");
 		System.out.println("opción 4: SMS");
-		System.out.println("opción 5: varias opciones");
-		System.out.println("Escoja una o varias opciones");
-
+		System.out.println("opción 5: SMS y Facebook");
+        
+		
+		//Acá verificamos que la opción sea válida
 		Scanner teclado = new Scanner(System.in);
-		int opcion = teclado.nextInt();
+		String opcion = teclado.next();
+	    if (opcion.equals("1") || opcion.equals("2") || opcion.equals("3") || opcion.equals("4") || opcion.equals("5") )  {
+			switch (opcion) {
+			
+			//Creamos los objetos de tipo notificador
+			case "1":
+				Notificador notificacion = new NotificadorFacebook(new EnviarNotificacion("\n" + mensaje));
+				System.out.println(notificacion.enviar());
+				break;
 
-		switch (opcion) {
-		case 1:
-			Notificador carro = new NotificadorFacebook(new EnviarNotificacion("\n" + mensaje));
-			System.out.println(carro.enviar());
-			break;
+			case "2":
+				Notificador notificacion1 = new NotificadorCorreoPersonal(new EnviarNotificacion("\n" + mensaje));
+				System.out.println(notificacion1.enviar());
+				break;
+			case "3":
+				Notificador notificacion2 = new NotificadorCorreoEmpresarial(new EnviarNotificacion("\n" + mensaje));
+				System.out.println(notificacion2.enviar());
+				break;
 
-		case 2:
-			Notificador carro1 = new NotificadorSMS(new EnviarNotificacion("\n" + mensaje));
-			System.out.println(carro1.enviar());
-			break;
-		case 3:
-			Notificador carro2 = new NotificadorCorreoEmpresarial(new EnviarNotificacion("\n" + mensaje));
-			System.out.println(carro2.enviar());
-			break;
+			case "4":
+				Notificador notificacion3 = new NotificadorSMS(new EnviarNotificacion("\n" + mensaje));
+				System.out.println(notificacion3.enviar());
+				break;
 
-		case 4:
-			Notificador carro3 = new NotificadorCorreoPersonal(new EnviarNotificacion("\n" + mensaje));
-			System.out.println(carro3.enviar());
-			break;
+			//Aquí hicimos un caso hipotético dado el caso de que el usuario quiera enviar el mensaje por Facebook o SMS
+			case "5":
+				Notificador notificacion4 = new NotificadorSMS(
+						new NotificadorFacebook(new EnviarNotificacion("\n" + mensaje)));
+				System.out.println(notificacion4.enviar());
+				break;
 
-		case 5:
-			Scanner teclado2 = new Scanner(System.in);
-			System.out.println("Ingrese el órden que quiera");
-			int opcion2 = teclado2.nextInt();
-			System.out.println("El órden es:" + opcion2);
-			break;
-
+			}
+		}else {
+			System.out.println("Escoja una opción valida");
 		}
+		
 
-		// String mensaje = teclado.nextLine(); System.out.print(mensaje);
-
-		// Notificador carro = new EnviarNotificacion("cualquier cosa");
-		// System.out.println(carro.enviar());
-
-		// Notificador carro = new NotificadorCorreoEmpresarial(new
-		// EnviarNotificacion(""));
-		// System.out.println(carro.enviar());
-
-		// Notificador carro = new NotificadorCorreoEmpresarial(new
-		// NotificadorFacebook(new EnviarNotificacion("")) );
-		// System.out.println(carro.enviar());
 	}
+	
 
 }
